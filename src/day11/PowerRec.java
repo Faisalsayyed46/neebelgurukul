3f467d400bcf05db3eca80300ac1aaf23474758b
Faisal Sayyed <faisalsayyad46@gmail.com>
2023-08-03 07:45:16 +0530

    day12

--- src/day11/PowerRec.java
+++ src/day11/PowerRec.java
@@ -2,4 +2,19 @@ package day11;
 
 public class PowerRec {
 
+    public static void main(String[] args) {
+	// TODO Auto-generated method stub
+	System.out.print(power(2, 5));
+    }
+
+    public static int power(int b, int p) {
+	if (p == 0) {
+	    return 1;
+	} else if (p == 1) {
+	    return b;
+	} else {
+	    return b * power(b, p - 1);
+	}
+    }
+
 }
